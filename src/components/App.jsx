6cc4c0fc77d37708b15670c16170b48eb6137595
Amrit <manthansoul@gmail.com>
2023-08-12 11:31:59 +0530

    created Carousel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { AdoptedPetContext } from "../contexts/AdoptedPetContext";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 function App() {
+  const adoptedPetHook = useState(null);
   return (
-    <div>
-	React-App
-    </div>
+    <BrowserRouter>
+      <AdoptedPetContext.Provider value={adoptedPetHook}>
+        <QueryClientProvider client={queryClient}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/" element={<SearchParams />} />
+            <Route path="/details/:id" element={<Details />} />
+          </Routes>
+        </QueryClientProvider>
+      </AdoptedPetContext.Provider>
+    </BrowserRouter>
   );
 }
 
