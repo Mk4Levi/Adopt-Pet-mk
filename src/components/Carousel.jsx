084735a1c3aecbdfc5fe0cc64bb37276c4999eb2
Manthan <mksoul1811@gmail.com>
2024-10-14 15:59:21 +0530

    Fix minor bugs

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,7 +6,7 @@ class Carousel extends Component {
   };
 
   static defaultProps = {
-    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+    images: ["https://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
   handleIndexClick = (event) => {
